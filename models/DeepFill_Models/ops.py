abe2ad07138669feab0d918e57c59ea0cc9c4fbb
Your Name <you@example.com>
2020-05-15 22:28:33 +0000

    tourchscript part 1

--- models/DeepFill_Models/ops.py
+++ models/DeepFill_Models/ops.py
@@ -1,9 +1,9 @@
 import math
-import numpy as np
+from typing import *
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 
 
 def weights_init(init_type='gaussian'):
@@ -55,7 +55,7 @@ class Conv_Downsample(nn.Module):
     def __init__(self, in_ch, out_ch, K=3, S=1, P=1, D=1, activation=nn.ELU()):
         super(Conv_Downsample, self).__init__()
 
-        PaddingLayer = torch.nn.ZeroPad2d((0, (K-1)//2, 0, (K-1)//2))
+        PaddingLayer = torch.nn.ZeroPad2d((0, (K - 1) // 2, 0, (K - 1) // 2))
 
         if activation is not None:
             self.conv = nn.Sequential(
@@ -91,18 +91,18 @@ class Down_Module(nn.Module):
         if isRefine:
             if isAttn:
                 layers.append(Conv_Downsample(curr_dim, curr_dim, K=3, S=2))
-                layers.append(Conv(curr_dim, 2*curr_dim, K=3, S=1))
-                layers.append(Conv_Downsample(2*curr_dim, 4*curr_dim, K=3, S=2))
+                layers.append(Conv(curr_dim, 2 * curr_dim, K=3, S=1))
+                layers.append(Conv_Downsample(2 * curr_dim, 4 * curr_dim, K=3, S=2))
                 layers.append(Conv(4 * curr_dim, 4 * curr_dim, K=3, S=1))
                 curr_dim *= 4
             else:
                 for i in range(2):
                     layers.append(Conv_Downsample(curr_dim, curr_dim, K=3, S=2))
-                    layers.append(Conv(curr_dim, curr_dim*2))
+                    layers.append(Conv(curr_dim, curr_dim * 2))
                     curr_dim *= 2
         else:
             for i in range(2):
-                layers.append(Conv_Downsample(curr_dim, curr_dim*2, K=3, S=2))
+                layers.append(Conv_Downsample(curr_dim, curr_dim * 2, K=3, S=2))
                 layers.append(Conv(curr_dim * 2, curr_dim * 2))
                 curr_dim *= 2
 
@@ -134,7 +134,7 @@ class Up_Module(nn.Module):
         layers = []
         curr_dim = in_ch
         if isRefine:
-            layers.append(Conv(curr_dim, curr_dim//2))
+            layers.append(Conv(curr_dim, curr_dim // 2))
             curr_dim //= 2
         else:
             layers.append(Conv(curr_dim, curr_dim))
@@ -143,11 +143,11 @@ class Up_Module(nn.Module):
         for i in range(2):
             layers.append(Conv(curr_dim, curr_dim))
             layers.append(nn.Upsample(scale_factor=2, mode='nearest'))
-            layers.append(Conv(curr_dim, curr_dim//2))
+            layers.append(Conv(curr_dim, curr_dim // 2))
             curr_dim //= 2
 
-        layers.append(Conv(curr_dim, curr_dim//2))
-        layers.append(Conv(curr_dim//2, out_ch, activation=None))
+        layers.append(Conv(curr_dim, curr_dim // 2))
+        layers.append(Conv(curr_dim // 2, out_ch, activation=None))
 
         self.out = nn.Sequential(*layers)
 
@@ -162,7 +162,7 @@ class Up_Module_CNet(nn.Module):
         layers = []
         curr_dim = in_ch
         if isRefine:
-            layers.append(Conv(curr_dim, curr_dim//2, isGated=isGated))
+            layers.append(Conv(curr_dim, curr_dim // 2, isGated=isGated))
             curr_dim //= 2
         else:
             layers.append(Conv(curr_dim, curr_dim, isGated=isGated))
@@ -171,11 +171,11 @@ class Up_Module_CNet(nn.Module):
         for i in range(2):
             layers.append(Conv(curr_dim, curr_dim, isGated=isGated))
             layers.append(nn.Upsample(scale_factor=2, mode='nearest'))
-            layers.append(Conv(curr_dim, curr_dim//2, isGated=isGated))
+            layers.append(Conv(curr_dim, curr_dim // 2, isGated=isGated))
             curr_dim //= 2
 
-        layers.append(Conv(curr_dim, curr_dim//2, isGated=isGated))
-        layers.append(Conv(curr_dim//2, out_ch, activation=None, isGated=isGated))
+        layers.append(Conv(curr_dim, curr_dim // 2, isGated=isGated))
+        layers.append(Conv(curr_dim // 2, out_ch, activation=None, isGated=isGated))
 
         self.out = nn.Sequential(*layers)
 
@@ -192,11 +192,11 @@ class Flatten_Module(nn.Module):
         curr_dim = out_ch
 
         for i in range(2):
-            layers.append(Conv(curr_dim, curr_dim*2, K=5, S=2, P=2, activation=nn.LeakyReLU()))
+            layers.append(Conv(curr_dim, curr_dim * 2, K=5, S=2, P=2, activation=nn.LeakyReLU()))
             curr_dim *= 2
 
         if isLocal:
-            layers.append(Conv(curr_dim, curr_dim*2, K=5, S=2, P=2, activation=nn.LeakyReLU()))
+            layers.append(Conv(curr_dim, curr_dim * 2, K=5, S=2, P=2, activation=nn.LeakyReLU()))
         else:
             layers.append(Conv(curr_dim, curr_dim, K=5, S=2, P=2, activation=nn.LeakyReLU()))
 
@@ -204,7 +204,7 @@ class Flatten_Module(nn.Module):
 
     def forward(self, x):
         x = self.out(x)
-        return x.view(x.size(0),-1) # 2B x 256*(256 or 512); front 256:16*16
+        return x.view(x.size(0), -1)  # 2B x 256*(256 or 512); front 256:16*16
 
 
 class Contextual_Attention_Module(nn.Module):
@@ -220,8 +220,9 @@ class Contextual_Attention_Module(nn.Module):
         self.isCheck = isCheck
         self.device = device
 
-    def forward(self, f, b, mask=None, ksize=3, stride=1,
-                fuse_k=3, softmax_scale=10., training=True, fuse=True):
+    def forward(self, f, b, mask: Optional[torch.Tensor] = None, ksize: int = 3, stride: int = 1,
+                fuse_k: int = 3, softmax_scale: float = 10., training: bool = True,
+                fuse: bool = True):
 
         """ Contextual attention layer implementation.
 
@@ -244,79 +245,88 @@ class Contextual_Attention_Module(nn.Module):
         """
 
         # get shapes
-        raw_fs = f.size() # B x 128 x 64 x 64
+        raw_fs = f.size()  # B x 128 x 64 x 64
         raw_int_fs = list(f.size())
         raw_int_bs = list(b.size())
 
         # extract patches from background with stride and rate
-        kernel = 2*self.rate
+        kernel = 2 * self.rate
         raw_w = self.extract_patches(b, kernel=kernel, stride=self.rate)
         raw_w = raw_w.permute(0, 2, 3, 4, 5, 1)
-        raw_w = raw_w.contiguous().view(raw_int_bs[0], raw_int_bs[2] // self.rate, raw_int_bs[3] // self.rate, -1)
+        raw_w = raw_w.contiguous().view(raw_int_bs[0], raw_int_bs[2] // self.rate,
+                                        raw_int_bs[3] // self.rate, -1)
         raw_w = raw_w.contiguous().view(raw_int_bs[0], -1, kernel, kernel, raw_int_bs[1])
         raw_w = raw_w.permute(0, 1, 4, 2, 3)
 
-        f = down_sample(f, scale_factor=1/self.rate, mode='nearest', device=self.device)
-        b = down_sample(b, scale_factor=1/self.rate, mode='nearest', device=self.device)
+        f = down_sample(f, scale_factor=1 / self.rate, mode='nearest', device=self.device)
+        b = down_sample(b, scale_factor=1 / self.rate, mode='nearest', device=self.device)
 
-        fs = f.size() # B x 128 x 32 x 32
+        fs = f.size()  # B x 128 x 32 x 32
         int_fs = list(f.size())
-        f_groups = torch.split(f, 1, dim=0) # Split tensors by batch dimension; tuple is returned
+        f_groups = torch.split(f, 1, dim=0)  # Split tensors by batch dimension; tuple is returned
 
         # from b(B*H*W*C) to w(b*k*k*c*h*w)
-        bs = b.size() # B x 128 x 32 x 32
+        bs = b.size()  # B x 128 x 32 x 32
         int_bs = list(b.size())
         w = self.extract_patches(b)
         w = w.permute(0, 2, 3, 4, 5, 1)
-        w = w.contiguous().view(raw_int_bs[0], raw_int_bs[2] // self.rate, raw_int_bs[3] // self.rate, -1)
+        w = w.contiguous().view(raw_int_bs[0], raw_int_bs[2] // self.rate,
+                                raw_int_bs[3] // self.rate, -1)
         w = w.contiguous().view(raw_int_bs[0], -1, ksize, ksize, raw_int_bs[1])
         w = w.permute(0, 1, 4, 2, 3)
         # process mask
-        mask = mask.clone()
         if mask is not None:
+            mask = mask.clone()
             if mask.size(2) != b.size(2):
-                mask = down_sample(mask, scale_factor=1./self.rate, mode='nearest', device=self.device)
+                mask = down_sample(mask, scale_factor=1. / self.rate, mode='nearest',
+                                   device=self.device)
         else:
             mask = torch.zeros([1, 1, bs[2], bs[3]])
 
         m = self.extract_patches(mask)
 
         m = m.permute(0, 2, 3, 4, 5, 1)
-        m = m.contiguous().view(raw_int_bs[0], raw_int_bs[2]//self.rate, raw_int_bs[3]//self.rate, -1)
+        m = m.contiguous().view(raw_int_bs[0], raw_int_bs[2] // self.rate,
+                                raw_int_bs[3] // self.rate, -1)
         m = m.contiguous().view(raw_int_bs[0], -1, ksize, ksize, 1)
         m = m.permute(0, 4, 1, 2, 3)
 
-        m = m[0] # (1, 32*32, 3, 3)
-        m = reduce_mean(m) # smoothing, maybe
-        mm = m.eq(0.).float() # (1, 32*32, 1, 1)
+        m = m[0]  # (1, 32*32, 3, 3)
+        m = reduce_mean(m)  # smoothing, maybe
+        mm = m.eq(0.).float()  # (1, 32*32, 1, 1)
 
-        w_groups = torch.split(w, 1, dim=0) # Split tensors by batch dimension; tuple is returned
-        raw_w_groups = torch.split(raw_w, 1, dim=0) # Split tensors by batch dimension; tuple is returned
+        w_groups = torch.split(w, 1, dim=0)  # Split tensors by batch dimension; tuple is returned
+        raw_w_groups = torch.split(raw_w, 1,
+                                   dim=0)  # Split tensors by batch dimension; tuple is returned
         y = []
         offsets = []
         k = fuse_k
         scale = softmax_scale
-        fuse_weight = Variable(torch.eye(k).view(1, 1, k, k)).cuda(self.device) # 1 x 1 x K x K
+        fuse_weight = torch.eye(k).view(1, 1, k, k).cuda()  # 1 x 1 x K x K
         y_test = []
         for xi, wi, raw_wi in zip(f_groups, w_groups, raw_w_groups):
             '''
             O => output channel as a conv filter
             I => input channel as a conv filter
             xi : separated tensor along batch dimension of front; (B=1, C=128, H=32, W=32)
-            wi : separated patch tensor along batch dimension of back; (B=1, O=32*32, I=128, KH=3, KW=3)
-            raw_wi : separated tensor along batch dimension of back; (B=1, I=32*32, O=128, KH=4, KW=4)
+            wi : separated patch tensor along batch dimension of back; (B=1, O=32*32, I=128, 
+            KH=3, KW=3)
+            raw_wi : separated tensor along batch dimension of back; (B=1, I=32*32, O=128, KH=4, 
+            KW=4)
             '''
             # conv for compare
             wi = wi[0]
-            escape_NaN = Variable(torch.FloatTensor([1e-4])).cuda(self.device)
+            escape_NaN = torch.tensor([1e-4], requires_grad=True, dtype=torch.double).cuda()
             wi_normed = wi / torch.max(l2_norm(wi), escape_NaN)
-            yi = F.conv2d(xi, wi_normed, stride=1, padding=1) # yi => (B=1, C=32*32, H=32, W=32)
+            yi = F.conv2d(xi, wi_normed, stride=1, padding=1)  # yi => (B=1, C=32*32, H=32, W=32)
             y_test.append(yi)
             # conv implementation for fuse scores to encourage large patches
             if fuse:
                 yi = yi.permute(0, 2, 3, 1)
                 yi = yi.contiguous().view(1, fs[2] * fs[3], bs[2] * bs[3], 1)
-                yi = yi.permute(0, 3, 1, 2)  # make all of depth to spatial resolution, (B=1, I=1, H=32*32, W=32*32)
+                yi = yi.permute(0, 3, 1,
+                                2)  # make all of depth to spatial resolution, (B=1, I=1,
+                # H=32*32, W=32*32)
                 yi = F.conv2d(yi, fuse_weight, stride=1, padding=1)  # (B=1, C=1, H=32*32, W=32*32)
 
                 yi = yi.permute(0, 2, 3, 1)
@@ -335,34 +345,35 @@ class Contextual_Attention_Module(nn.Module):
             else:
                 yi = yi.permute(0, 2, 3, 1)
                 yi = yi.contiguous().view(1, fs[2], fs[3], bs[2] * bs[3])
-                yi = yi.permute(0, 3, 1, 2) # (B=1, C=32*32, H=32, W=32)
+                yi = yi.permute(0, 3, 1, 2)  # (B=1, C=32*32, H=32, W=32)
                 # yi = yi.contiguous().view(1, bs[2] * bs[3], fs[2], fs[3])
 
             # softmax to match
             yi = yi * mm  # mm => (1, 32*32, 1, 1)
-            yi = F.softmax(yi*scale, dim=1)
+            yi = F.softmax(yi * scale, dim=1)
             yi = yi * mm  # mask
 
-            _, offset = torch.max(yi, dim=1) # argmax; index
+            _, offset = torch.max(yi, dim=1)  # argmax; index
             division = torch.div(offset, fs[3]).long()
-            offset = torch.stack([division, torch.div(offset, fs[3])-division], dim=-1)
+            offset = torch.stack([division, torch.div(offset, fs[3]) - division], dim=-1)
 
             wi_center = raw_wi[0]
 
-            yi = F.conv_transpose2d(yi, wi_center, stride=self.rate, padding=1) / 4. # (B=1, C=128, H=64, W=64)
+            yi = F.conv_transpose2d(yi, wi_center, stride=self.rate,
+                                    padding=1) / 4.  # (B=1, C=128, H=64, W=64)
             y.append(yi)
             offsets.append(offset)
 
-        y = torch.cat(y, dim=0) # back to the mini-batch
+        y = torch.cat(y, dim=0)  # back to the mini-batch
         y.contiguous().view(raw_int_fs)
         # wi_patched = y
         offsets = torch.cat(offsets, dim=0)
         offsets = offsets.view([int_bs[0]] + [2] + int_bs[2:])
 
         # case1: visualize optical flow: minus current position
-        h_add = Variable(torch.arange(0,float(bs[2]))).cuda(self.device).view([1, 1, bs[2], 1])
+        h_add = torch.arange(0, float(bs[2])).cuda().view([1, 1, bs[2], 1])
         h_add = h_add.expand(bs[0], 1, bs[2], bs[3])
-        w_add = Variable(torch.arange(0,float(bs[3]))).cuda(self.device).view([1, 1, 1, bs[3]])
+        w_add = torch.arange(0, float(bs[3])).cuda().view([1, 1, 1, bs[3]])
         w_add = w_add.expand(bs[0], 1, bs[2], bs[3])
 
         offsets = offsets - torch.cat([h_add, w_add], dim=1).long()
@@ -373,7 +384,7 @@ class Contextual_Attention_Module(nn.Module):
 
         return y, offsets
 
-    def extract_patches(self, x, kernel=3, stride=1):
+    def extract_patches(self, x, kernel: int = 3, stride: int = 1):
         x = self.padding(x)
         all_patches = x.unfold(2, kernel, stride).unfold(3, kernel, stride)
 
@@ -382,26 +393,30 @@ class Contextual_Attention_Module(nn.Module):
 
 def reduce_mean(x):
     for i in range(4):
-        if i==1: continue
+        if i == 1:
+            continue
         x = torch.mean(x, dim=i, keepdim=True)
     return x
 
 
-def l2_norm(x):
-    def reduce_sum(x):
-        for i in range(4):
-            if i==0: continue
-            x = torch.sum(x, dim=i, keepdim=True)
-        return x
+def reduce_sum(x):
+    for i in range(4):
+        if i == 0:
+            continue
+        x = torch.sum(x, dim=i, keepdim=True)
+    return x
 
-    x = x**2
+
+def l2_norm(x):
+    x = x ** 2
     x = reduce_sum(x)
     return torch.sqrt(x)
 
-    
+
 # Pytorch>=0.4.1, when use the old down_sample func, I found some problems in the results
 # to check the reason
-def down_sample(x, size=None, scale_factor=None, mode='nearest', device=None):
+def down_sample(x, scale_factor: Optional[float] = None, mode: str = 'nearest',
+                device: Optional[str] = None):
     res = F.interpolate(x, scale_factor=scale_factor, mode=mode)
     return res
 
@@ -421,9 +436,9 @@ def down_sample(x, size=None, scale_factor=None, mode='nearest', device=None):
 #     grid = grid.unsqueeze(0).repeat(x.size(0),1,1,1)
 #     if x.is_cuda:
 #         if device:
-#             grid = Variable(grid).cuda(device)
+#             grid = torch.tensor(grid).cuda(device)
 #         else:
-#             grid = Variable(grid).cuda()
+#             grid = torch.tensor(grid).cuda()
 #     # do sampling
 
 #     return F.grid_sample(x, grid, mode=mode)
@@ -435,4 +450,4 @@ def to_var(x, volatile=False, device=None):
             x = x.cuda(device)
         else:
             x = x.cuda()
-    return Variable(x, volatile=volatile)
+    return torch.tensor(x, requires_grad=True, volatile=volatile)
